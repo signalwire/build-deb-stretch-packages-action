1395ed63ea31b38a9b2edca35a900fe4216c8daf
Lucas Mellos Carlos <lucasmelloscarlos@gmail.com>
2023-02-09 21:48:42 -0300

    init action

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VERSION=`dpkg-parsechangelog --show-field Version | cut -f1 -d"-"`
+HASH=`echo $REVISION | cut -c1-10`
+dch -b -M -v "$VERSION-$GITHUB_RUN_ID~$GITHUB_SHA~`lsb_release -cs`" --force-distribution -D "`lsb_release -cs`" "Nightly build, $GITHUB_SHA" 
+debuild -b -us -uc 
+ls -la ..
+
+mkdir -p $GITHUB_WORKSPACE/$OS_FAMILY
+mv ../*.deb $GITHUB_WORKSPACE/$OS_FAMILY
